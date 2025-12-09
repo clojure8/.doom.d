da6a7ed1f25b7da4ae02f3eba7ac13f31acb2e28
wupingteng <wupingteng@jtexpress.com>
2025-12-09 18:38:24 +0800

    修改 awesome-tray 配置

--- modules/my/awesome-tray/config.el
+++ modules/my/awesome-tray/config.el
@@ -19,21 +19,18 @@
                   magit-status-mode-hook
                   magit-log-mode-hook))
 
-    (add-hook hook #'+my-hide-special-mode-line-h))
+    (add-hook hook #'+my-hide-special-mode-line-h)))
 
-  (add-hook! 'magit-post-refresh-hook #'hide-mode-line-mode))
 
 
 (use-package! awesome-tray
+  :after (hide-mode-line)
   :config
   ;; Configuration for awesome-tray appearance
   (setq awesome-tray-mode-line-active-color "#5B6268")
   (setq awesome-tray-mode-line-height 0.1) ; Set height to minimal
   (setq awesome-tray-active-modules
         '("evil" "buffer-name" "file-path" "git" "mode"))
-
-  ;; Update modules when awesome-tray is already enabled
-  (add-hook 'doom-load-theme-hook #'awesome-tray-update)
 
   ;; Fix "double line" and "height" issues (critical step)
   ;; Doom themes add :box to mode-line, we need to remove it
@@ -55,8 +52,6 @@
   ;; Apply fix when loading awesome-tray
   (add-hook 'awesome-tray-mode-hook #'fix-awesome-tray-modeline-face)
 
-  ;; Enable the plugin
-  (awesome-tray-mode 1))
 
-;; Hook to ensure both modes are active when window configuration changes
-(add-hook! 'window-configuration-change-hook #'hide-mode-line-mode #'awesome-tray-mode)
+  ;; Hook to ensure both modes are active when window configuration changes
+  (add-hook! 'window-configuration-change-hook #'hide-mode-line-mode #'awesome-tray-mode))
