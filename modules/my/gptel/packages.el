42db345ea30cc5acebbb7a97eff733b149df05f3
wupingteng <wupingteng@jtexpress.com>
2025-12-09 20:32:35 +0800

    remove sort-tab

--- modules/my/gptel/packages.el
+++ modules/my/gptel/packages.el
@@ -1,10 +1,4 @@
 ;;; my/gptel/packages.el -*- lexical-binding: t; -*-
 (package! gptel :recipe (:nonrecursive t))
 
-(package! sort-tab
-  :recipe (:host github
-           :repo "manateelazycat/sort-tab"
-           :branch "main"
-           :files ("*.el")
-           ;; do not perform byte compilation or native compilation for lsp-bridge
-           :build (:not compile)))
+
