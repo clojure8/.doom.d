73fa0b888b0e426fa095d9d0c917976ea5f13319
wupingteng <wupingteng@jtexpress.com>
2025-12-11 01:28:27 +0800

    移除 apikey

--- modules/my/chatgpt-shell/config.el
+++ modules/my/chatgpt-shell/config.el
@@ -5,4 +5,4 @@
 
   ;; 可选：设为默认模型
   ;; (setq chatgpt-shell-model-version "glm-4")
-  (setq chatgpt-shell-zhipu-key "abc7499e887a45818d951fb52ba58307.BEK5gqURdrEHF3Os"))
+  (setq chatgpt-shell-zhipu-key (getenv "CHATGPT_SHELL_ZHIPU_KEY")))
